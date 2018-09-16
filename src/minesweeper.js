4edc50fa5fec14c1839c4ccff377f3829f743f40
Kelly Pierduta <kelly@kpdigitalstrategy.com>
2018-09-16 15:54:34 +0100

    Second stage of project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,12 @@
-const blankLine = '  |   |  ';
-
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-const guessLine = '1 |   |  ';
-
-const bombLine = '  | B |  ';
-console.log('This is what a board with a guess and a bomb on it would look like');
-
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const printBoard = board => {
+console.log(printBoard);
+console.log(board[0].join(' | '));
+console.log(board[1].join(' | '));
+console.log(board[2].join(' | '));
+};
+const board = [
+[' ', ' ', ' '],
+[' ', ' ', ' '],
+[' ', ' ', ' ']
+];
+printBoard(board);
